d5a580556ac959cac15fc8e2f55ac1c6760cdb64
LisIva <lizavvveta99@gmail.com>
2025-03-05 19:55:38 +0300

    Create a new class 'OptManager' to module the optimization steps

---
+++ pipeline/optimization_workflow/optimization_manager.py
@@ -0,0 +1,75 @@
+from pipeline.optimization_workflow.evaluator import Evaluator
+from pipeline.buffer_handler.eq_buffer import EqBuffer
+from pipeline.get_llm_response import get_response, get_debug_response
+from pipeline.rebuild_prompt import rebuild_prompt
+from pipeline.buffer_handler.eq_pruner import Pruner
+from pipeline.clean_directories import clean_output_dir, reset_prompt_to_init
+from tqdm import tqdm
+import sys
+import traceback
+
+
+class OptManager:
+    def __init__(self, max_iter, start_iter, refine_point=100, dir_name='burg', debug=True, print_exc=True,
+                 exit_code=True, resample_shape=(20, 20), n_candidates=4):
+        self.exit_code = exit_code
+        self.debug = debug
+        self.print_exc = print_exc
+        self.refine_point = refine_point
+        self.start_iter = start_iter
+        self.max_iter = max_iter
+        self.dir_name = dir_name
+
+        self.evaluator = Evaluator(dir_name, resample_shape)
+        self.eq_buffer = EqBuffer()
+        self._pruner = None
+        self._n_pruner_cand = n_candidates
+
+    def call_pruner(self):
+        self._pruner = Pruner(self.eq_buffer, self.evaluator, self._n_pruner_cand)
+
+    def explore_solutions(self):
+        self.step_0()
+        for num in tqdm(range(self.start_iter, min(self.refine_point, self.max_iter)), desc="LLM's progress"):
+            new_prompt, score, str_equation, params = self.step_n("prompts/continue-iter.txt", num)
+
+    def refine_solutions(self):
+        for num in tqdm(range(min(self.refine_point, self.start_iter), self.max_iter), desc="LLM's progress"):
+            new_prompt, score, str_equation, params = self.step_n("prompts/continue-iter-refinement.txt", num)
+
+    def step_0(self):
+        if self.start_iter == 0:
+            while True:
+                try:
+                    if not self.debug:
+                        clean_output_dir()
+                    reset_prompt_to_init()
+                    new_prompt, score, str_equation, params = self.perform_step(path="prompts/zero-iter.txt", num=0)
+                    self.start_iter = 1
+                    break
+                except Exception as e:
+                    print('An exception occurred on iter #0:')
+                    print(traceback.format_exc())
+                    if self.exit_code: sys.exit()
+
+    def step_n(self, path, num=1):
+        try:
+            new_prompt, score, str_equation, params = self.perform_step(path, num=num)
+        except Exception as e:
+            print(f"\nException occurred on iter #{num}:")
+            if self.print_exc:
+                # EOL while scanning string literal
+                print(traceback.format_exc())
+            if self.exit_code:
+                sys.exit()
+            return None, None, None, None
+        return new_prompt, score, str_equation, params
+
+    def perform_step(self, path, num):
+        if self.debug:
+            response = get_debug_response(num=num)
+        else:
+            response = get_response(prompt_path=path, num=num, dir_name=self.dir_name, print_info=False)
+        score, eq_string, params = self.evaluator.llm_response_eval(response, self.eq_buffer)
+        new_prompt, old_prompt = rebuild_prompt(eq_string, score, response, num=num, path=path)
+        return new_prompt, score, eq_string, params
--- pipeline/pipeline_main.py
+++ pipeline/pipeline_main.py
@@ -1,78 +1,20 @@
-from evaluator import piped_evaluator
-from buffer_handler.eq_buffer import EqBuffer
-from get_llm_response import get_response, get_debug_response
-from rebuild_prompt import rebuild_prompt
-from buffer_handler.eq_pruner import Pruner
-from clean_directories import clean_output_dir, reset_prompt_to_init
-from tqdm import tqdm
-import sys
-import traceback
+from pipeline.optimization_workflow.optimization_manager import OptManager
 
-MAX_ITER = 8
-DIR_NAME = 'sindy-burg'
-START_ITER = 0
-REFINE_POINT = 100
+max_iter = 8
+dir_name = 'sindy-burg'
+start_iter = 0
+refine_point = 100
 
-DEBUG = True # True False
-PRINT_EXC = True
-EXIT = True
-
-resample_shape = (20, 20)
-eq_buffer = EqBuffer()
-
-def perform_step(path, num, debug=False):
-    if debug:
-        response = get_debug_response(num=num)
-    else:
-        response = get_response(prompt_path=path, num=num, dir_name=DIR_NAME, print_info=False)
-    score, eq_string, params = piped_evaluator(response, eq_buffer, DIR_NAME)
-    new_prompt, old_prompt = rebuild_prompt(eq_string, score, response, num=num, path=path)
-    return new_prompt, score, eq_string, params
-
-
-def step(path, num=0, debug=False):
-    try:
-        new_prompt, score, str_equation, params = perform_step(path, num=num, debug=debug)
-    except Exception as e:
-        print(f"\nException occurred on iter #{num}:")
-        if PRINT_EXC:
-            # EOL while scanning string literal
-            print(traceback.format_exc())
-        if EXIT:
-            sys.exit()
-        return None, None, None, None
-    return new_prompt, score, str_equation, params
-
-
-def step_0(path="prompts/zero-iter.txt", debug=False):
-    new_prompt, score, str_equation, params = perform_step(path, num=0, debug=debug)
-    return new_prompt, score, str_equation, params
+debug = True # True False
+print_exc = True
+exit_code = True
 
 
 if __name__ == '__main__':
-
-
-    if START_ITER == 0:
-        while True:
-            try:
-                if not DEBUG:
-                    clean_output_dir()
-                reset_prompt_to_init()
-                new_prompt, score, str_equation, params = step_0(debug=DEBUG)
-                START_ITER = 1
-                break
-            except Exception as e:
-                print('An exception occurred on iter #0:')
-                print(traceback.format_exc())
-                if EXIT: sys.exit()
-
-    for num in tqdm(range(START_ITER, min(REFINE_POINT, MAX_ITER)), desc="LLM's progress"):
-        new_prompt, score, str_equation, params = step("prompts/continue-iter.txt", num, debug=DEBUG)
-
-    # for num in tqdm(range(min(REFINE_POINT, START_ITER), MAX_ITER), desc="LLM's progress"):
-    #     new_prompt, score, str_equation, params = step("prompts/continue-iter-refinement.txt", num, debug=DEBUG)
-
-    pruner = Pruner(eq_buffer.full_opt_track, eq_buffer.records_track, 4)
+    opt_manager = OptManager(max_iter, start_iter, refine_point, dir_name, debug, print_exc, exit_code,
+                             resample_shape=(20, 20), n_candidates=4)
+    opt_manager.explore_solutions()
+    opt_manager.call_pruner()
     print()
 
 
